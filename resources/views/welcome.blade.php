53fb45100e69d66940af3df7a8d8fc851b8bcc0a
airat <airat@onlbp.ru>
2020-07-05 10:29:49 +0300

    локальные стили quasar на cdn

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -9,16 +9,18 @@
 
     <!-- Fonts -->
     <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
-    <!-- <link href="https://fonts.googleapis.com/css?family=Roboto:100,300,400,500,700,900|Material+Icons" rel="stylesheet" 
-        type="text/css"> -->
-    <!-- <link href="https://cdn.jsdelivr.net/npm/quasar@1.12.8/dist/quasar.min.css" rel="stylesheet" type="text/css"> -->
-    <link href="{{ asset('css/quasar.min.css') }}" rel="stylesheet" type="text/css">
-    <link href="{{ asset('css/roboto.css') }}" rel="stylesheet" type="text/css">
+    <link href="https://fonts.googleapis.com/css?family=Roboto:100,300,400,500,700,900|Material+Icons" rel="stylesheet" 
+        type="text/css"> 
+    <link href="https://cdn.jsdelivr.net/npm/quasar@1.12.8/dist/quasar.min.css" rel="stylesheet" type="text/css">
+    <!-- <link href="{{ asset('css/quasar.min.css') }}" rel="stylesheet" type="text/css">
+    <link href="{{ asset('css/roboto.css') }}" rel="stylesheet" type="text/css"> -->
 
 </head>
 
 <body>
     <app id="q-app"></app>
+    <script src="https://cdn.jsdelivr.net/npm/vue@^2.0.0/dist/vue.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/quasar@1.12.8/dist/quasar.umd.min.js"></script>
     <script src="{{ asset('js/app.js') }}"></script>
 </body>
 
